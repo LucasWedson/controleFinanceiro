6ff63fab919547093db9a42ab8137497d3602777
Lucas Wedson <lwedson12@gmail.com>
2022-04-21 20:03:59 -0300

    Meu primeiro commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const dummyTransactions = [
+  {id: 1, name: 'Bolo de brigadeiro', amount: -20},
+  {id: 2, name: 'Salário', amount: 300},
+  {id: 3, name: 'Torta de frango', amount: -20},
+  {id: 4, name: 'Violão', amount: 150}
+]
+
+const addTransactionIntoDOM = transaction =>{
+  const operator = transaction.amount < 0 ? '-' : '+'
+  const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
+  const li = document.createElement('li')
+
+  li.classList.add(CSSClass)
+  li.innerHTML = `
+    ${transaction.name} <span>-$400</span><button class="delete-btn">x</button>
+  `
+  console.log(li)
+
+}
+addTransactionIntoDOM(dummyTransactions[0])
