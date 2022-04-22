59dc689610e532c29729b38f51c000df0f943b77
Lucas Wedson <lwedson12@gmail.com>
2022-04-21 22:12:46 -0300

    Entrega parte das receitas, despesas, total e inserção de novas transações

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-const transactionUl = document.querySelector('#transaction')
+const transactionUl = document.querySelector('#transactions')
+const incomeDisplay = document.querySelector('#money-plus')
+const expenseDisplay = document.querySelector('#money-minus')
+const balanceDisplay = document.querySelector('#balance')
+const form = document.querySelector('form')
+const inputTransactionName = document.querySelector('#text')
+const inputTransactionAmount = document.querySelector('#amount')
 
 
 const dummyTransactions = [
   {id: 1, name: 'Bolo de brigadeiro', amount: -20},
   {id: 2, name: 'Salário', amount: 300},
-  {id: 3, name: 'Torta de frango', amount: -20},
+  {id: 3, name: 'Torta de frango', amount: -10},
   {id: 4, name: 'Violão', amount: 150}
 ]
 
@@ -18,7 +24,53 @@ const addTransactionIntoDOM = transaction =>{
   li.innerHTML = `
     ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">x</button>
   `
-  transactionUl.append(li);
+  transactionUl.prepend(li);
 
 }
-addTransactionIntoDOM(dummyTransactions[0])
+const updateBalanceValues = () =>{
+  const transactionsAmounts = dummyTransactions
+    .map(transaction => transaction.amount)
+  const total = transactionsAmounts
+    .reduce((accumulator, transaction) => accumulator + transaction, 0)
+  const income = transactionsAmounts
+    .filter(value => value > 0)
+    .reduce((accumulator, value) => accumulator + value, 0)
+    .toFixed(2)
+
+  const expense = Math.abs(transactionsAmounts
+    .filter(value => value < 0)
+    .reduce((accumulator, value) => accumulator + value, 0))
+    .toFixed(2)
+
+  balanceDisplay.textContent = `R$ ${total}`
+  incomeDisplay.textContent = `R$ ${income}`
+  expenseDisplay.textContent = `R$ ${expense}`
+ 
+}
+
+const init = ()=>{
+  dummyTransactions.forEach(addTransactionIntoDOM)
+  updateBalanceValues()
+}
+init()
+
+const generateID = () => Math.round(Math.random() * 1000)
+
+
+form.addEventListener('submit',event => {
+  event.preventDefault()
+
+  const transactionName = inputTransactionName.value.trim()
+  const transactionAmount = inputTransactionAmount.value.trim()
+
+
+  if(transactionName === '' || transactionAmount === ''){
+    alert(' Por favor, preencha tanto o nome quanto o valor da transação')
+    return
+  }
+
+  const transaction = {id: generateID(), name: transactionName, amount: transactionAmount}
+
+  console.log(transaction)
+
+})
